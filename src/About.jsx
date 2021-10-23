fc2b13e13e03d37949b2822a4ef6beb30a22b9a6
Subham <paras123hzb@gmail.com>
2021-10-23 17:35:25 +0530

    add

---
+++ src/About.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import web from "../src/images/hero-img.png";
+import Commom from "./Common";
+
+const About = () => {
+  return (
+    <>
+      <Commom
+        name="Welcome to About page"
+        imgsrc={web}
+        visit="/contact"
+        btname="Contact Now"
+      />
+    </>
+  );
+};
+export default About;
---
+++ src/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="w-100  bg-transparent text-center  p-2">
+        <p>© 2021 SubhamTech. All Rights Reserved</p>
+      </footer>
+    </>
+  );
+};
+export default Footer;
